5a6ffc32d7f40770a91623c93479b420d9b91de2
Nicolas <nicolas.mardesson@gmail.com>
2022-08-23 00:51:25 +0200

    password secret

--- kubernetes_dag.py
+++ kubernetes_dag.py
@@ -1,7 +1,19 @@
 from airflow import DAG
+from airflow.kubernetes.secret import Secret
 from datetime import datetime, timedelta
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.operators.dummy_operator import DummyOperator
+
+secret_env = Secret(
+    # Expose the secret as environment variable.
+    deploy_type='env',
+    # The name of the environment variable, since deploy_type is `env` rather
+    # than `volume`.
+    deploy_target='TARGET_POSTGRES_PASSWORD',
+    # Name of the Kubernetes Secret
+    secret='postgresql-dev',
+    # Key of a secret stored in this Secret object
+    key='postgres-password')
 
 default_args = {
     'owner': 'airflow',
@@ -15,7 +27,7 @@ default_args = {
 }
 
 dag = DAG(
-    'kubernetes_spreadsheets_to_postgres', default_args=default_args, schedule_interval=timedelta(minutes=10))
+    'kubernetes_spreadsheets_to_postgres', default_args=default_args, schedule_interval=timedelta(days=1))
 
 
 start = DummyOperator(task_id='start', dag=dag)
@@ -23,7 +35,8 @@ start = DummyOperator(task_id='start', dag=dag)
 passing = KubernetesPodOperator(namespace='airflow',
                           image="meltano-project:dev",
                           arguments=["elt", "tap-spreadsheets-anywhere", "target-postgres", "--transform=skip"],
-                          env_vars={"TARGET_POSTGRES_PASSWORD":"mysecretadminpassword"},
+                          # env_vars={"TARGET_POSTGRES_PASSWORD":"mysecretadminpassword"},
+                          secrets=[secret_env],
                           name="spreadsheets-to-postgres",
                           task_id="spreadsheets-to-postgres-task",
                           get_logs=True,
